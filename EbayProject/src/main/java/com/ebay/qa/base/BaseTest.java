697a397bcac8f14270ac3f7c11a22e51446dafa6
Sushila Goyat <sush.rinwa@gmail.com>
2019-08-20 16:49:53 -0600

    commit

--- EbayProject/src/main/java/com/ebay/qa/base/BaseTest.java
+++ EbayProject/src/main/java/com/ebay/qa/base/BaseTest.java
@@ -1,5 +1,53 @@
 package com.ebay.qa.base;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.ebay.qa.util.TestUtil;
+
 public class BaseTest {
+	
+	static WebDriver driver;
+	static Properties prop;
+	
+	public BaseTest() {
+		prop=new Properties();
+		FileInputStream fis=null;
+		try {
+			fis = new FileInputStream("C:\\Users\\parmod.kumar\\git\\AmazonTest\\AmazonTest\\src\\main\\java\\com\\amazon\\qa\\config\\config.properties");
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		try {
+			prop.load(fis);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+	
+	public void initialization() {
+		String browserName=prop.getProperty("browser");
+		if(browserName.equals("chrome")) {
+            System.setProperty("webdriver.chrome.driver", "C:/mytools/chromedriver.exe");
+			
+			//launching chrome browser
+			driver=new ChromeDriver();
+		}else {
+System.setProperty("webdriver.gecko.driver", "C:/mytools/geckodriver.exe");
+			
+			//launching chrome browser
+			driver=new FirefoxDriver();
+		}
+		driver.manage().timeouts().pageLoadTimeout(TestUtil.page_load_timeout, TimeUnit.SECONDS);
+	}
 
 }
---
+++ EbayProject/src/main/java/com/ebay/qa/util/TestUtil.java
@@ -0,0 +1,7 @@
+package com.ebay.qa.util;
+
+public class TestUtil {
+	public static int page_load_timeout=20;
+	static int implicit_wait=20;
+
+}
